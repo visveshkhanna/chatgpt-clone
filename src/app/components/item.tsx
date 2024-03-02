4846622a7c7edab9da6b5227792010e36356c3fb
visveshkhanna <visveshkhanna@gmail.com>
2024-03-02 23:20:21 +0530

    Flex row changes

--- src/app/components/item.tsx
+++ src/app/components/item.tsx
@@ -4,9 +4,9 @@ import { RiArchive2Line } from "react-icons/ri";
 export function HistoryItem(
     {title} : {title: string}
 ) {
-    return <div className={"group flex flex-row items-center overflow-hidden w-full py-3 text-sm whitespace-nowrap px-2 hover:bg-accent-foreground/5 cursor-pointer rounded-xl"}>
+    return <div className={"group flex flex-row items-center justify-between overflow-hidden w-full py-3 text-sm whitespace-nowrap px-2 hover:bg-accent-foreground/5 cursor-pointer rounded-xl"}>
         <span>{title}</span>
-        <div className={"flex-row space-x-1 absolute right-12 hidden group-hover:flex py-4 items-center"}>
+        <div className={"sticky flex-row space-x-1 hidden group-hover:flex items-center"}>
             <BsThreeDots className="w-4 h-4 hover:opacity-50" />
             <RiArchive2Line className="w-4 h-4 hover:opacity-50" />
         </div>
